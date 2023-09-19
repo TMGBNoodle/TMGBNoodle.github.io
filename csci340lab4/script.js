6286acd0d9749fe5f77f6e57d722e1c88a410d0c
TMGBNoodle <grovesbenedicttm@hendrix.edu>
2023-09-18 20:18:40 -0500

    Made Website

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,27 +1,66 @@
 $(document).ready(function(){
-    var Objects;
-    $ajax({
-        dataType: "json",
-        url: "https://rps101.pythonanywhere.com/api/v1/objects/all",
-        success: function(results){
-            console.log(results[url]);
-            Objects = results[url];
-        },
-        error: function(xhr,status,error) {
-            console.log(error);
+    var Money = 
+    $("#showCat").click(function() {
+        $.ajax({
+            dataType: "json",
+            url: "https://api.thecatapi.com/v1/images/search",
+            success: function(results) {
+                console.log(results[0].url);
+                $('#cat').attr("src", results[0].url);
+            },
+            error: function(xhr,status,error) {
+              console.log(error);
+            }
+          });
+    });
+    $("#showDog").click(function() {
+        $.ajax({
+            dataType: "json",
+            url: "https://random.dog/woof.json",
+            success: function(results) {
+              console.log(results["url"]);
+              if (!results["url"].endsWith(".mp4")) {
+                $('#dog').attr("src", results["url"]);
+              }
+            },
+            error: function(xhr,status,error) {
+              console.log(error);
+            }
+          });
+    });
+    $("#doRPS").click(function() {
+        var choice1 = Math.floor(Math.random() * 100);
+        console.log(choice1);
+        if(choice1 <= 33){
+            choice1 = 1;
+            console.log("Cat Rock");
+            $("#catChoice").text("Cats chose Rock!");
+        }else if (choice1 <= 66){
+            choice1 = 2;
+            $("#catChoice").text("Cats chose Paper!");
+        } else {
+            choice1 = 3;
+            $("#catChoice").text("Cats chose Scissors!");
+        }
+        var choice2 = Math.floor(Math.random() * 100);
+        if(choice2 <= 33){
+            choice2 = 1;
+            $("#dogChoice").text("Dogs chose Rock!");
+        }else if (choice2 <= 66){
+            choice2 = 2;
+            $("#dogChoice").text("Dogs chose Paper!");
+        } else {
+            choice2 = 3;
+            $("#dogChoice").text("Dogs chose Scissors!");
+        }
+        console.log(choice2-choice1);
+        if(choice2 - choice1 == 1 || choice2 - choice1 == -2){
+            $("#winner").text("Dogs Win!");
+
+        } else if (choice2 == choice1){
+            $("#winner").text("Draw");
+        } else{
+            $("#winner").text("Cats Win!");
         }
     });
-    $("#showCases").click(function() {
-        var display = $("#Display")
-        var displayVal = [];
-        while(display.length < 10){
-            var random = math.floor(math.random()*101);
-            if(!Objects[random] in display){
-                displayVal.push(Objects[random])
-            }
-        }
-        for (let index = 0; index < array.length; index++) {
-            display.text(display.text + ", " +displayVal[i]);
-        }
-    })
-})
+});
